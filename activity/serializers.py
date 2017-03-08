7cc85b6c1d58b305fb2db72adf2643c5c105db9b
Aniket Prakash <aniket.prakash79@gmail.com>
2017-03-09 04:06:36 +0530

    Added activity

---
+++ activity/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from activity.models import Activity
+
+
+class Activityserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = ('name','date','added_by','amount')
