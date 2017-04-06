e7345f8223c5594b5aa398c04ff234d6754edcb8
Aniket Prakash <aniket.prakash79@gmail.com>
2017-04-07 00:49:07 +0530

    added imagefield

--- Customer/models.py
+++ Customer/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
-from django.db import models
-# Create your models here.
-from Taxxo import settings
+
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db import models
+
+
+def user_directory_path(instance, filename):
+    return 'email_{0}/{1}'.format(instance.email, filename)
 
 
 class Profile(AbstractUser, PermissionsMixin):
     number = models.IntegerField(blank=True, default=10)
     birth_date = models.DateField(null=True, blank=True)
     is_superuser = models.BooleanField()
+    image = models.ImageField(upload_to=user_directory_path)
