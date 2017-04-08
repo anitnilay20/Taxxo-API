70a1765409cd1b77635353577c830e47299905ab
Aniket Prakash <aniket.prakash79@gmail.com>
2017-04-08 16:49:34 +0530

    added balance sheet

--- trialbalance/views.py
+++ trialbalance/views.py
@@ -2,12 +2,11 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from trialbalance.models import TrialBalance
-from trialbalance.serializers import TrialBalanceSerializers
-
 from expense.models import Expense
 from income.models import Income
-import json
+from ledgers.models import Ledgers
+from trialbalance.models import TrialBalance
+from trialbalance.serializers import TrialBalanceSerializers
 
 
 class TrialBalanceList(APIView):
@@ -37,10 +36,10 @@ class ProfitLoss(APIView):
             else:
                 income_data[i.secondAccount.name] = i.amount
         print income_data
-        for key,item in income_data.iteritems():
+        for key, item in income_data.iteritems():
             final_income = {}
-            final_income['particular']=key
-            final_income['amount']=item
+            final_income['particular'] = key
+            final_income['amount'] = item
             incomes.append(final_income)
 
         for i in expense:
@@ -49,12 +48,28 @@ class ProfitLoss(APIView):
             else:
                 expense_data[i.secondAccount.name] = i.amount
 
-        for key,item in expense_data.iteritems():
+        for key, item in expense_data.iteritems():
             final_expense = {}
-            final_expense['particular']=key
-            final_expense['amount']=item
+            final_expense['particular'] = key
+            final_expense['amount'] = item
             expenses.append(final_expense)
         data = dict()
-        data['income']=incomes
-        data['expense']=expenses
+        data['income'] = incomes
+        data['expense'] = expenses
+        return JsonResponse(data, safe=False)
+
+
+class Balancesheet(APIView):
+    def get(self, request, format=None):
+        current_assets = Ledgers.objects.filter(
+            groups=['bank OCC AC', 'cash in hand', 'deposits assets', 'loans n advances assets', 'stock in hand',
+                    'sundry debitors'])
+        loans_liability = Ledgers.objects.filter(groups=['bank OD AC', 'secured loans', 'unsecured loans'])
+        curent_liabilities = Ledgers.objects.filter(groups=['duties n taxes', 'provisions', 'sundry creditors'])
+        capital_account = Ledgers.objects.filter(groups=['reserve n surpulus'])
+        data = dict()
+        data['current_assets'] = current_assets
+        data['loans_liability'] = loans_liability
+        data['curent_liabilities'] = curent_liabilities
+        data['capital_account'] = capital_account
         return JsonResponse(data, safe=False)
