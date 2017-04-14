68bd24f39e69622f6e0eeed6425ff090ddfd80c6
Anit Nilay <anit.nilay@scripbox.com>
2017-04-15 01:01:03 +0530

    update serializer for journal

--- journal/serializers.py
+++ journal/serializers.py
@@ -5,5 +5,4 @@ from .models import Journal
 class Journalserializers(serializers.ModelSerializer):
     class Meta:
         model = Journal
-        fields = ('credit_amount','debit_amount','narration','for_account','to_Account'
-            )
+        fields = ('credit_amount','debit_amount','narration','for_account','to_Account','company','date')
