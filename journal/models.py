6006c31f58e8053833dad0ea1831162320942468
Anit Nilay <anit.nilay@scripbox.com>
2017-04-14 10:47:06 +0530

    bugfix

--- journal/models.py
+++ journal/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 from django.db import models
+from ledgers.models import 
 
 
 class Journal(models.Model):
     credit_amount = models.IntegerField()
     debit_amount = models.IntegerField()
     narration = models.CharField(max_length=1000)
-    credit_account = models.CharField(max_length=1000)
-    debit_account = models.CharField(max_length=1000)
+    foraccount = models.ForeignKey(Ledgers)
+    toAccount = models.ForeignKey(Ledgers)
+    date = models.DateField()
