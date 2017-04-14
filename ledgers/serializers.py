a57482000f8248b5936f875ba68c0dbf710621c2
Anit <anit.nilay20@gmail.com>
2017-04-14 12:27:39 +0530

    modified ledgers model

--- ledgers/serializers.py
+++ ledgers/serializers.py
@@ -5,4 +5,4 @@ from ledgers.models import Ledgers
 class Ledgersserializers(serializers.ModelSerializer):
     class Meta:
         model = Ledgers
-        fields = ('id','company','name','groups','opening_balance','type','inventory','date','credit_account','debit_account')
+        fields = ('id','company','name','groups','opening_balance','type','inventory','date','credit_amount','debit_amount')
