d82fba90fb23df3402ab08c744e3c18abbf4cc23
Anit <anit.nilay20@gmail.com>
2017-05-14 13:24:58 +0530

    Update sales and purchase

--- sales/models.py
+++ sales/models.py
@@ -2,12 +2,13 @@ from __future__ import unicode_literals
 from django.db import models
 from journal.models import Journal
 from Customer.models import Profile
+from django.utils import timezone
 
 
 class Sales(models.Model):
-    Invoice = models.IntegerField()
     party_name = models.CharField(max_length=1000)
-    reference = models.IntegerField()
+    date = models.DateField(default=timezone.now)
     journals = models.ManyToManyField(Journal,related_name='juurnal')
     payment_method = models.CharField(max_length=1000)
+    total_amount = models.IntegerField()
     added_by = models.ForeignKey(Profile)
