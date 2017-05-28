58552261f293d5c5c8e1f7b39745ca93f883129f
Aniketprax <aniket.prakash79@gmail.com>
2017-05-28 12:50:25 +0530

    modify settings

--- Taxxo/urls.py
+++ Taxxo/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-                  url(r'^user/', admin.site.urls),
                   url(r'^company/', include('company.urls')),
                   url(r'^customer/', include('Customer.urls')),
                   url(r'^ledgers/', include('ledgers.urls')),
@@ -13,5 +12,7 @@ urlpatterns = [
                   url(r'^admin/', include(admin.site.urls)),
                   url(r'^purchase/', include('purchase.urls')),
                   url(r'^sales/', include('sales.urls')),
+                  url(r'^ledgerhistory/', include('ledgerhistory.urls')),
+                  url(r'^product/', include('product.urls')),
                   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
