2c6a8c792c27cde9ebf6af52e14a431aba476407
Aniketprax <aniket.prakash79@gmail.com>
2017-05-29 00:33:17 +0530

    add manytomanyfield

--- ledgerhistory/models.py
+++ ledgerhistory/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 
 class LedgerHistory(models.Model):
     name = models.ForeignKey(Ledgers, related_name='journalsname')
-    type = models.IntegerField()
-    to_account = models.ForeignKey(Ledgers, related_name='journalstoaccount')
+    type = models.CharField(max_length=1000)
+    to_account = models.ManyToManyField(Ledgers, related_name='journalstoaccount')
     date = models.DateTimeField(default=timezone.now)
     closing_balance = models.IntegerField()
     total_amount = models.IntegerField()
